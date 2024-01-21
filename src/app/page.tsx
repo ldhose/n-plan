4a351821e18afc2b81b6791e599d423496cf3b98
ldhose <eldhosemjoy@outlook.com>
2024-01-21 17:02:39 +0530

    Added prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,107 @@
-'use client'
-import Image from 'next/image'
-import { Timer } from '../components/timer'
-import { useRef, useState } from 'react';
-import { useInterval } from "../components/utils"
-
-
+"use client";
+import Image from "next/image";
+import { Timer } from "../components/timer";
+import { useRef, useState } from "react";
+import { useInterval } from "../components/utils";
 
 export type TimerInstance = {
-  tag: string,
-  hour: number,
-  minute: number,
-  second: number,
-  expired: boolean,
-  notified: boolean
-}
-
+  tag: string;
+  hour: number;
+  minute: number;
+  second: number;
+  expired: boolean;
+  notified: boolean;
+};
 
 export default function Home() {
+  const [timer, setTimer] = useState<TimerInstance>({
+    tag: "Timer",
+    second: 0,
+    minute: 0,
+    hour: 0,
+    expired: false,
+    notified: false,
+  });
 
-  const [timer, setTimer] = useState<TimerInstance>({tag: "Timer", second: 0, minute: 0, hour: 0, expired: false, notified: false});
+  function updateTimer(): void {
+    if (timer.expired) {
+      if (!timer.notified) {
+        console.log("Notification sent");
+        // notify({text: "timer expired"})
+        //TODO notify
+      }
 
-
-
-  function updateTimer(): void {    
-    if(timer.expired) {
-        if(!timer.notified) {
-            console.log("Notification sent")
-            // notify({text: "timer expired"})
-                //TODO notify   
-        }
-
-
-        setTimer({...timer, ...{expired:true, notified: true}})
-        return
-    }    
-    var timerStateCopy: TimerInstance = structuredClone(timer)
-    updateTimerSecond(timerStateCopy)
-    console.log("updating time")
-
-    setTimer(timerStateCopy)
-}
-
-function updateTimerHour(timer: TimerInstance) {
-    if(timer.hour == 0) {
-        timer.minute = 0
-        timer.hour =0
-        timer.second = 0
-        timer.expired = true
-    } else {
-        timer.hour = timer.hour - 1
-        timer.minute = 59
+      setTimer({ ...timer, ...{ expired: true, notified: true } });
+      return;
     }
-    return timer
-}
+    var timerStateCopy: TimerInstance = structuredClone(timer);
+    updateTimerSecond(timerStateCopy);
+    console.log("updating time");
 
-function updateTimerMinute(timer: TimerInstance) {
-    if(timer.minute == 0) {
-        updateTimerHour(timer)
-        
+    setTimer(timerStateCopy);
+  }
+
+  function updateTimerHour(timer: TimerInstance) {
+    if (timer.hour == 0) {
+      timer.minute = 0;
+      timer.hour = 0;
+      timer.second = 0;
+      timer.expired = true;
     } else {
-        timer.minute = timer.minute - 1 
-        timer.second = 59
+      timer.hour = timer.hour - 1;
+      timer.minute = 59;
     }
-    return timer
+    return timer;
+  }
 
-}
-
-function updateTimerSecond(timer: TimerInstance): TimerInstance{
-    if(timer.second == 0) {
-        updateTimerMinute(timer)
+  function updateTimerMinute(timer: TimerInstance) {
+    if (timer.minute == 0) {
+      updateTimerHour(timer);
     } else {
-        timer.second = timer.second - 1
+      timer.minute = timer.minute - 1;
+      timer.second = 59;
     }
-    return timer
-}
-useInterval(() => updateTimer(), 1000)
+    return timer;
+  }
 
-function startTimer() {
-  const updatedTimer: TimerInstance = {...timer, ...{minute: 5, second: 0, expired: false, notified: false}}
-  setTimer(updatedTimer)
-  console.log(timer)
-}
+  function updateTimerSecond(timer: TimerInstance): TimerInstance {
+    if (timer.second == 0) {
+      updateTimerMinute(timer);
+    } else {
+      timer.second = timer.second - 1;
+    }
+    return timer;
+  }
+  useInterval(() => updateTimer(), 1000);
+
+  function startTimer() {
+    const updatedTimer: TimerInstance = {
+      ...timer,
+      ...{ minute: 5, second: 0, expired: false, notified: false },
+    };
+    setTimer(updatedTimer);
+    console.log(timer);
+  }
 
   return (
     <main className="min-h-screen min-w-full bg-slate-900 p-6">
-      <div className="m-w-64 m-auto p-1
-                         md:m-w-96">
-          <input type='text' className='w-full min-h-9 block m-auto text-center bg-slate-800'>
-          </input>
-          <button id="5min-button" onClick={startTimer}> 5 min</button>
-      {/* <div className='grid '>
+      <div
+        className="m-w-64 m-auto p-1
+                         md:m-w-96"
+      >
+        <input
+          type="text"
+          className="w-full min-h-9 block m-auto text-center bg-slate-800"
+        ></input>
+        <button id="5min-button" onClick={startTimer}>
+          {" "}
+          5 min
+        </button>
+        {/* <div className='grid '>
         <button className=''>Start Timer</button>
       </div> */}
       </div>
-      { !timer.notified &&
-      <Timer timer={timer}/>
-}
-
+      {!timer.notified && <Timer timer={timer} />}
     </main>
-  )
+  );
 }
-
--- src/components/Notification.ts
+++ src/components/Notification.ts
@@ -1,20 +1,17 @@
 type NotificationItem = {
-    text: string
-}
+  text: string;
+};
 
-export const notify = ({text}: NotificationItem) => {
-    if(!("Notification" in window)) {
-        console.log("This browser doesn't support desktop notification")
-    } else if (Notification.permission === "granted") {
-        new Notification("text")
-    } else if (Notification.permission !== "denied") {
-        Notification.requestPermission().then(function (permission) {
-            if(permission === "granted") {
-                new Notification("text")
-            }
-        })
-    }
-
-
-
-}
+export const notify = ({ text }: NotificationItem) => {
+  if (!("Notification" in window)) {
+    console.log("This browser doesn't support desktop notification");
+  } else if (Notification.permission === "granted") {
+    new Notification("text");
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        new Notification("text");
+      }
+    });
+  }
+};
